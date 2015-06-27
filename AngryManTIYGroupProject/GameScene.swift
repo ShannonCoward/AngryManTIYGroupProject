9536309a6c849b9988ca1ce7799a635d083e9a21
ShannonArmon <shannon.armon@gmail.com>
2015-06-27 02:28:11 -0400

    Character Add

--- AngryManTIYGroupProject/GameScene.swift
+++ AngryManTIYGroupProject/GameScene.swift
@@ -9,35 +9,38 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    
+    
+    var ceo: SKSpriteNode!
+    
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+
+        ceo = childNodeWithName("ceo") as! SKSpriteNode
         
-        self.addChild(myLabel)
     }
     
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+   override func touchesBegan(touches: Set<NSObject>, withEvent: UIEvent)  {
+    
+    
+            let juniorDeveloper = SKShapeNode(fileNamed: "juniorDeveloper")
+            juniorDeveloper.physicsBody = SKPhysicsBody.self
+                juniorDeveloper.position = CGPointMake(200, 100)
+    
+                        addChild(juniorDeveloper)
+    
+                juniorDeveloper.physicsBody?.applyImpulse(CGVectorMake(500, 300))
+    
+    
+    
+    
         
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
-    }
+        
+
+
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
